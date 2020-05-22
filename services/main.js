b874c7800a078ccb70c5fcf753fb641f237ad2e4
Nykolle Malone <nykolle.malone@hotmail.com>
2020-05-22 07:10:29 -0300

    style improved

--- services/main.js
+++ services/main.js
@@ -1,64 +1,108 @@
-fetch('https://sky-frontend.herokuapp.com/movies')
-    .then(res => res.json()).then(json => {
-        json[2].movies.forEach(item => {
-            let idx = item.categories.indexOf("Ação")
-            if(idx != -1){
-                $(".acao").append(
-                   `<div class="swiper-slide">
-                    <img class="swiper-img" src="${item.images[0].url}" alt="${item.title}">
+fetch("https://sky-frontend.herokuapp.com/movies")
+  .then((res) => res.json())
+  .then((data) => {
+    data[2].movies.forEach((movie) => {
+      let idx = movie.categories.indexOf("Ação");
+      if (idx != -1) {
+        $(".acao").append(
+          `<div class="swiper-slide">
+                    <img class="swiper-img" src="${movie.images[0].url}" alt="${movie.title}">
                     </div>`
-                )
-            }
-        })
-    }).catch(() => {
-        $('.card-deck').html('Nenhum resultado encontrado!')
-    })
+        );
+      }
+    });
+  })
+  .catch(() => {
+    $(".acao").html("Nenhum resultado encontrado!");
+  });
 
-//     fetch('https://sky-frontend.herokuapp.com/movies')
-//     .then(res => res.json()).then(json => {
-//         json[2].movies.forEach(item => {
-//             let idx = item.categories.indexOf("Infantil")
-//             if(idx != -1){
-//                 $(".infantil").prepend(
-//                     `<div class="card">
-//                         <img class="img-fluid" src="${item.images[0].url}" alt="${item.title}">
-//                     </div>`
-//                 )
-//             }
-//         })
-//     }).catch(() => {
-//         $('.card-deck').html('Nenhum resultado encontrado!')
-//     })
-
-    fetch('https://sky-frontend.herokuapp.com/movies')
-    .then(res => res.json()).then(json => {
-        json[2].movies.forEach(item => {
-            let idx = item.categories.indexOf("Terror")
-            if(idx != -1){
-                $(".terror").prepend(
-                    `<div class="swiper-slide">
-                    <img class="swiper-img" src="${item.images[0].url}" alt="${item.title}">
+fetch("https://sky-frontend.herokuapp.com/movies")
+  .then((res) => res.json())
+  .then((data) => {
+    data[2].movies.forEach((movie) => {
+      let idx = movie.categories.indexOf("Infantil");
+      if (idx != -1) {
+        $(".infantil").append(
+          `<div class="swiper-slide">
+                    <img class="swiper-img" src="${movie.images[0].url}" alt="${movie.title}">
                     </div>`
-                )
-            }
-        })
-    }).catch(() => {
-        $('.card-deck').html('Nenhum resultado encontrado!')
-    })
+        );
+      }
+    });
+  })
+  .catch(() => {
+    $(".infantil").html("Nenhum resultado encontrado!");
+  });
 
-//     fetch('https://sky-frontend.herokuapp.com/movies')
-//     .then(res => res.json()).then(json => {
-//         json[2].movies.forEach(item => {
-//             let idx = item.categories.indexOf("Drama")
-//             if(idx != -1){
-//                 $(".drama").prepend(
-//                     `<div class="card">
-//                         <img class="img-fluid" src="${item.images[0].url}" alt="${item.title}">
-//                     </div>`
-//                 )
-//             }
-//         })
-//     }).catch(() => {
-//         $('.card-deck').html('Nenhum resultado encontrado!')
-//     })
+fetch("https://sky-frontend.herokuapp.com/movies")
+  .then((res) => res.json())
+  .then((data) => {
+    data[2].movies.forEach((movie) => {
+      let idx = movie.categories.indexOf("Comédia");
+      if (idx != -1) {
+        $(".comedia").append(
+          `<div class="swiper-slide">
+                    <img class="swiper-img" src="${movie.images[0].url}" alt="${movie.title}">
+                    </div>`
+        );
+      }
+    });
+  })
+  .catch(() => {
+    $(".comedia").html("Nenhum resultado encontrado!");
+  });
+
+fetch("https://sky-frontend.herokuapp.com/movies")
+  .then((res) => res.json())
+  .then((data) => {
+    data[2].movies.forEach((movie) => {
+      let idx = movie.categories.indexOf("Drama");
+      if (idx != -1) {
+        $(".drama").append(
+          `<div class="swiper-slide">
+                    <img class="swiper-img" src="${movie.images[0].url}" alt="${movie.title}">
+                    </div>`
+        );
+      }
+    });
+  })
+  .catch(() => {
+    $(".drama").html("Nenhum resultado encontrado!");
+  });
+
+fetch("https://sky-frontend.herokuapp.com/movies")
+  .then((res) => res.json())
+  .then((data) => {
+    data[2].movies.forEach((movie) => {
+      let idx = movie.categories.indexOf("Terror" && "Suspense");
+      if (idx != -1) {
+        $(".terror").append(
+          `<div class="swiper-slide">
+                    <img class="swiper-img" src="${movie.images[0].url}" alt="${movie.title}">
+                    </div>`
+        );
+      }
+    });
+  })
+  .catch(() => {
+    $(".terror").html("Nenhum resultado encontrado!");
+  });
+
+fetch("https://sky-frontend.herokuapp.com/movies")
+  .then((res) => res.json())
+  .then((data) => {
+    data[2].movies.forEach((movie) => {
+      let idx = movie.categories.indexOf("Ficção Científica");
+      if (idx != -1) {
+        $(".ficcao").append(
+          `<div class="swiper-slide">
+                    <img class="swiper-img" src="${movie.images[0].url}" alt="${movie.title}">
+                    </div>`
+        );
+      }
+    });
+  })
+  .catch(() => {
+    $(".ficcao").html("Nenhum resultado encontrado!");
+  });
 
